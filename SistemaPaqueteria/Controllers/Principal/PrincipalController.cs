30b878a59131291931988360de1872f4d94dd82d
julio ismael gonzalez quijano <juliuz_274@hotmail.com>
2021-05-11 12:30:27 -0500

    clientes,vista,dto,controler

---
+++ SistemaPaqueteria/Controllers/Principal/PrincipalController.cs
@@ -0,0 +1,43 @@
+﻿using SistemaPaqueteria.Models;
+using SistemaPaqueteria.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaPaqueteria.Controllers.Principal
+{
+    public class PrincipalController : Controller
+    {
+        // GET: Principal
+        public ActionResult Principal()
+        {
+            List<ClientesDTO> lst;
+            using (LOGPACKEntities db = new LOGPACKEntities())
+            {
+                lst = (from d in db.tbl_Clientes
+                           select new ClientesDTO
+                           {
+                               iIdCliente = d.iIdCliente,
+                               cRfc = d.cRfc,
+                               cMoralFisica = d.cMoralFisica,
+                               cNombres = d.cNombres,
+                               cApellidos = d.cApellidos,
+                               iClienteFacturazion = d.iClienteFacturacion,
+                               iTipoCliente = d.iTipoCliente,
+                               cCorreoElectronico = d.cCorreoElectronico,
+                               cDireccion = d.cDireccion,
+                               nTelefono = d.nTelefono,
+                               iCp = d.iCp,
+                               cColonia = d.cColonia,
+                               cUsuario = d.cUsuario,
+                               cContraseña = d.cContraseña,
+                               iMetodoPagoEspecial = d.iMetodoPagoEspecial
+                           }).ToList();
+            }
+
+                return View(lst);
+        }
+    }
+}
---
+++ SistemaPaqueteria/Models/DTO/ClientesDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaPaqueteria.Models.DTO
+{
+    public class ClientesDTO
+    {
+        public int iIdCliente { get; set; }
+
+        public string cRfc { get; set; }
+
+        public string cMoralFisica { get; set; }
+
+        public string cNombres { get; set; }
+
+        public string cApellidos { get; set; }
+
+        public int iClienteFacturazion { get; set; }
+
+        public int iTipoCliente { get; set; }
+
+        public string cCorreoElectronico { get; set; }
+
+        public string cDireccion { get; set; }
+
+        public decimal nTelefono { get; set; }
+
+        public int iCp { get; set; }
+
+        public string cColonia { get; set; }
+
+        public string cUsuario { get; set; }
+
+        public string cContraseña { get; set; }
+
+        public int iMetodoPagoEspecial { get; set; }
+    }
+}
